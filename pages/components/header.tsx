408aeffe2ba26f6b5722d124cb23dce4f9f036af
xiikii <andy864940012@gmail.com>
2023-06-13 21:17:52 +0800

    fix:reject visit outside ndjy

--- pages/components/header.tsx
+++ pages/components/header.tsx
@@ -1,9 +1,4 @@
-import { mincuCore, uiModule } from "mincu-react";
-
-import { Button } from "@geist-ui/core";
 import Link from "next/link";
-
-const isApp = mincuCore.isApp;
 
 const HeaderComponent = () => {
   return (
@@ -11,19 +6,6 @@ const HeaderComponent = () => {
       <h2>Beta_Copilot</h2>
       <Link href="./">Go to Index Page</Link>
       <Link href="./mine">Go to My Page</Link>
-      {/* {isApp ? (
-        <Link href="./mine">Go to My Page</Link>
-      ) : (
-        <Button
-          onClick={() => {
-            window.location.href = `incu://Webview?url=${encodeURIComponent(
-              "http://localhost:3000/mine"
-            )}`;
-          }}
-        >
-          Go to My Page
-        </Button>
-      )} */}
     </div>
   );
 };
